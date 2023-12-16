1a14f6744eb0774c11287157e875e6c5fbc1717b
pedroMF1996 <pmfrp@hotmail.com>
2023-12-16 15:15:22 -0300

    IbgeModelMapping criado

---
+++ src/Balta.Localizacao.MVVM.Data/Mappings/IbgeModelMapping.cs
@@ -0,0 +1,44 @@
+﻿using Balta.Localizacao.MVVM.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Balta.Localizacao.MVVM.Data.Mappings
+{
+    public class IbgeModelMapping : IEntityTypeConfiguration<IbgeModel>
+    {
+        public void Configure(EntityTypeBuilder<IbgeModel> builder)
+        {
+            builder.HasKey(x => x.Id)
+                .HasName("PK_IBGE");
+
+            builder.Property(x => x.Id)
+                .HasColumnType("char")
+                .HasMaxLength(7)
+                .IsRequired();
+
+            builder.Property(x => x.City) 
+                .HasColumnType("nvarchar")
+                .HasMaxLength(80)
+                .IsRequired();
+
+            builder.Property(x => x.State)
+                .HasColumnType("char")
+                .HasMaxLength (2)
+                .IsRequired();
+
+            builder.HasIndex(x => x.Id)
+                .IsUnique()
+                .HasName("IX_IBGE_Id");
+
+            builder.HasIndex(x => x.City)
+                .IsUnique()
+                .HasName("IX_IBGE_City");
+            
+            builder.HasIndex(x => x.State)
+                .IsUnique()
+                .HasName("IX_IBGE_State");
+
+            builder.ToTable("IBGE");
+        }
+    }
+}
--- src/Balta.Localizacao.MVVM.Domain/Models/Validations/IbgeModelValidation.cs
+++ src/Balta.Localizacao.MVVM.Domain/Models/Validations/IbgeModelValidation.cs
@@ -9,7 +9,7 @@ namespace Balta.Localizacao.MVVM.Domain.Models.Validations
         public static readonly string IdLengthErrorMessage = "O campo Codigo deve conter 7 caracteres.";
         public static readonly string IdOnlyNumbersErrorMessage = "O campo Codigo deve conter apenas numeros.";
         public static readonly string CityRequiredErrorMessage = "O campo Cidade é obrigatorio.";
-        public static readonly string CityMaxLengthErrorMessage = "O campo Cidade deve conter até 150 caracteres.";
+        public static readonly string CityMaxLengthErrorMessage = "O campo Cidade deve conter até 80 caracteres.";
         public static readonly string StateRequiredErrorMessage = "O campo Estado é obrigatorio.";
         public static readonly string StateLengthErrorMessage = "O código Estado conter até 2 caracteres.";
 
@@ -25,7 +25,7 @@ namespace Balta.Localizacao.MVVM.Domain.Models.Validations
             RuleFor(x => x.City)
                 .NotEmpty()
                 .WithMessage(CityRequiredErrorMessage)
-                .MaximumLength(150)
+                .MaximumLength(80)
                 .WithMessage(CityMaxLengthErrorMessage);
             RuleFor(x => x.State)
                 .NotEmpty()
