2f067c173c3f849aa31181d82b78a4625b5787f1
SilvioCarlos12 <silvio.net.ma@gmail.com>
2023-12-16 15:12:27 -0300

    ajustado na pasta de IbgeViewModel, trocado nome e colocado viewModel de atualização

--- src/Balta.Localizacao.MVVM.PresentetionLayer/ViewModels/IbgeViewModels/IbgeAdicionarViewModel.cs
+++ src/Balta.Localizacao.MVVM.PresentetionLayer/ViewModels/IbgeViewModels/IbgeAdicionarViewModel.cs
@@ -1,9 +1,9 @@
 ﻿using Balta.Localizacao.MVVM.Core.Presentaion;
 using Balta.Localizacao.MVVM.Domain.Models;
 
-namespace Balta.Localizacao.MVVM.PresentetionLayer.ViewModels
+namespace Balta.Localizacao.MVVM.PresentetionLayer.ViewModels.IbgeViewModels
 {
-    public sealed class IbgeAdiconarViewModel:BaseViewModel<IbgeModel>
+    public sealed class IbgeAdicionarViewModel : BaseViewModel<IbgeModel>
     {
         public string Id { get; set; }
         public string State { get; set; }
---
+++ src/Balta.Localizacao.MVVM.PresentetionLayer/ViewModels/IbgeViewModels/IbgeAtualizarViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Balta.Localizacao.MVVM.Core.Presentaion;
+using Balta.Localizacao.MVVM.Domain.Models;
+
+namespace Balta.Localizacao.MVVM.PresentetionLayer.ViewModels.IbgeViewModels
+{
+    public sealed class IbgeAtualizarViewModel:BaseViewModel<IbgeModel>
+    {
+        public string Id { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+    }
+}
