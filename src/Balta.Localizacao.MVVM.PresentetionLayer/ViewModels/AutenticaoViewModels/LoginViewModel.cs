2f117e7cb122064489262b74725ea73c4815c7a5
SilvioCarlos12 <silvio.net.ma@gmail.com>
2023-12-16 15:27:29 -0300

    adicionado ViewModel Login

---
+++ src/Balta.Localizacao.MVVM.PresentetionLayer/ViewModels/AutenticaoViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Balta.Localizacao.MVVM.Core.Presentaion;
+using Balta.Localizacao.MVVM.Domain.Models;
+
+namespace Balta.Localizacao.MVVM.PresentetionLayer.ViewModels.AutenticaoViewModels
+{
+    public sealed class LoginViewModel:BaseViewModel<IbgeModel>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
