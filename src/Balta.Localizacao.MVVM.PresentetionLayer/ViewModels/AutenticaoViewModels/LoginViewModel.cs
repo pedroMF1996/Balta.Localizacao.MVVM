e91f162167c9e2bd74aeb7e28bb02d1ab3bf8a34
SilvioCarlos12 <silvio.net.ma@gmail.com>
2023-12-16 15:53:04 -0300

    adiconado  validação com dataAnnotions

--- src/Balta.Localizacao.MVVM.PresentetionLayer/ViewModels/AutenticaoViewModels/LoginViewModel.cs
+++ src/Balta.Localizacao.MVVM.PresentetionLayer/ViewModels/AutenticaoViewModels/LoginViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using Balta.Localizacao.MVVM.Core.Presentaion;
 using Balta.Localizacao.MVVM.Domain.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Balta.Localizacao.MVVM.PresentetionLayer.ViewModels.AutenticaoViewModels
 {
     public sealed class LoginViewModel:BaseViewModel<IbgeModel>
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
